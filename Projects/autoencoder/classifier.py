4a2104b1843b16ebbcc2eec439ae5a1780ad7137
darshandodamani <darshanmd.dn@gmail.com>
2024-09-07 00:03:18 +0200

    classifier added for testing

---
+++ Projects/autoencoder/classifier.py
@@ -0,0 +1,19 @@
+# Projects/autoencoder/classifier.py
+
+import torch
+import torch.nn as nn
+
+# Define a simple feedforward classifier
+class ClassifierModel(nn.Module):
+    def __init__(self, input_size=256, hidden_size=128, output_size=2):
+        super(ClassifierModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)  # No activation here, we will use CrossEntropyLoss that expects raw logits
+        return x
