e18fdee6737c3bbb54095295be9c261b72111fe3
darshandodamani <darshanmd.dn@gmail.com>
2024-12-02 18:58:19 +0100

    m basd mag maskng

---
+++ Projects/masking/lime_on_images/lime_based_image_masking.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from sympy import per
+import torch
+import random
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import cv2
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from PIL import Image
+from skimage.metrics import structural_similarity as ssim, mean_squared_error as mse, peak_signal_noise_ratio as psnr
+from sewar.full_ref import vifp, uqi
+from lime.lime_tabular import LimeTabularExplainer
+import torchvision.transforms as transforms
+
+# Add Python path to include the directory where 'encoder.py' is located
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "autoencoder"))
+)
+
+from encoder import VariationalEncoder
+from decoder import Decoder
+from classifier import ClassifierModel as Classifier
+from vae import VariationalAutoencoder, CustomImageDatasetWithLabels
+
+# Paths to models
+encoder_path = "model/epochs_500_latent_128_town_7/var_encoder_model.pth"
+decoder_path = "model/epochs_500_latent_128_town_7/decoder_model.pth"
+classifier_path = "model/epochs_500_latent_128_town_7/classifier_final.pth"
+
+# Load the models
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+encoder = VariationalEncoder(latent_dims=128, num_epochs=100).to(device)
+decoder = Decoder(latent_dims=128, num_epochs=100).to(device)
+classifier = Classifier().to(device)
+
+encoder.load_state_dict(torch.load(encoder_path, map_location=device, weights_only=True))
+decoder.load_state_dict(torch.load(decoder_path, map_location=device, weights_only=True))
+classifier.load_state_dict(torch.load(classifier_path, map_location=device, weights_only=True))
+
+encoder.eval()
+decoder.eval()
+classifier.eval()
+
